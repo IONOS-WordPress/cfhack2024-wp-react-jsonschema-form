a1022d18eac0d7a4bbe51119028e80aea2998103
lars.gersmann <lars.gersmann@ionos.com>
2024-02-26 22:57:40 +0100

    fix: corrected a few typos in .wp-env-afterStart.sh

--- .wp-env-afterStart.sh
+++ .wp-env-afterStart.sh
@@ -5,12 +5,12 @@
 # this script is used to customize the created wp-env instance
 #
 
-WPENV_INSTALLPATH="$(realpath --relative-to $(pwd) $(pnpm run --silent  wp-env install-path))"
+WPENV_INSTALLPATH="$(realpath --relative-to $(pwd) $(pnpm run --silent wp-env install-path))"
 
 # build gutenberg for development (for debugging purposes - i.e. including source maps etc.)
 pushd "${WPENV_INSTALLPATH}/gutenberg"
 # compute path to our configured nodejs provided by pnpm environment
-NODE_PATH="$(dirname $(which node))/..";
+NODE_PATH="$(dirname $(which node))/.."
 # calling the original npm delivered with the configured nodejs version is a bit quirky but works :-)
 $NODE_PATH/bin/node "$NODE_PATH/lib/node_modules/npm/bin/npm-cli.js" ci
 $NODE_PATH/bin/node "$NODE_PATH/lib/node_modules/npm/bin/npm-cli.js" run build:packages
