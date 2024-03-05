f76dbbf72c22bcd0df2fe870aa2710cdb5146f9f
Lars Gersmann <lars.gersmann@cm4all.com>
2024-03-05 14:27:26 +0100

    refactor: fixed rjsf import

--- packages/wp-content/plugins/react-jsonschema-form-renderer/src/rjsf-core/rjsf-core.js
+++ packages/wp-content/plugins/react-jsonschema-form-renderer/src/rjsf-core/rjsf-core.js
@@ -1,4 +1,4 @@
-import * as Exports from './../../node_modules/@rjsf/core/dist/index.esm.js';
+import * as Exports from './../../node_modules/@rjsf/core/lib/index.js';
 
 window['rjsf']??={};
 window['rjsf']['core']=Exports;
