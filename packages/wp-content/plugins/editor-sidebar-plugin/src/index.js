f569f6a297c6af8f84824a9259e30d56654a5a39
Lars Gersmann <lars.gersmann@gmail.com>
2024-03-13 14:08:22 +0100

    fix: fixed editor plugin registration

--- packages/wp-content/plugins/editor-sidebar-plugin/src/index.js
+++ packages/wp-content/plugins/editor-sidebar-plugin/src/index.js
@@ -1,11 +1,12 @@
 import { registerPlugin } from '@wordpress/plugins';
 import Sidebar from './components/sidebar.js';
+import { Icon } from '@wordpress/components';
 
 import './index.scss';
 
-import icon from '../../../../../shared/src/hackathon-icon.js';
+import HackathonIcon from './../../../../../shared/src/hackathon-icon.js';
 
 registerPlugin( 'editor-sidebar-plugin', {
-	icon,
+	icon : <Icon icon={ HackathonIcon }/>,
 	render: Sidebar,
 } );
