0e1cb3c3bdf9f90f1133d8ad70a1d44431137541
Maximilian Andre <maximilian.andre@e-mundo.de>
2024-03-16 18:38:35 +0100

    fix(PasswordWidget): Changed back to a non experimental TextControl

--- packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/widgets/PasswordWidget.jsx
+++ packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/widgets/PasswordWidget.jsx
@@ -1,4 +1,4 @@
-import {__experimentalInputControl as InputControl} from '@wordpress/components';
+import { TextControl } from '@wordpress/components';
 
 /**
  *
@@ -6,5 +6,5 @@ import {__experimentalInputControl as InputControl} from '@wordpress/components'
  */
 export default function PasswordWidget(props) {
   const {options, registry, schema, onBlur} = props;
-  return <InputControl type='password' className={"password-widget"} label={schema.title} placeholder={options.help} {...props} />;
+  return <TextControl type={'password'} className={"password-widget"} label={schema.title} placeholder={options.help} onChange={props.onChange} />;
 }
