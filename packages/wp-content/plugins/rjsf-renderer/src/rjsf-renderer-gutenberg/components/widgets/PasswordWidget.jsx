58ba5da1c1cac0b36bfd3226429dc33021893a31
Maximilian Andre <maximilian.andre@e-mundo.de>
2024-03-16 16:42:36 +0100

    feat(rjsx-renderer): Changed Password input to InputControl

--- packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/widgets/PasswordWidget.jsx
+++ packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/widgets/PasswordWidget.jsx
@@ -1,10 +1,10 @@
-import { getTemplate } from '@rjsf/utils';
-/** The `PasswordWidget` component uses the `BaseInputTemplate` changing the type to `password`.
+import {__experimentalInputControl as InputControl} from '@wordpress/components';
+
+/**
  *
  * @param props - The `WidgetProps` for this component
  */
 export default function PasswordWidget(props) {
-    const { options, registry } = props;
-    const BaseInputTemplate = getTemplate('BaseInputTemplate', registry, options);
-    return <BaseInputTemplate type='password' {...props}/>;
+  const {options, registry, schema, onBlur} = props;
+  return <InputControl type='password' label={schema.title} placeholder={options.help} {...props} />;
 }
