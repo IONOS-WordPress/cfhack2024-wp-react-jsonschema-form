008f766808b77d336a3f9b2e5652dc20caba80ad
anja <anja.lang@live.de>
2024-03-17 17:58:20 +0100

    fix react warning and remove unused

--- packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/templates/ButtonTemplates/AddButton.jsx
+++ packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/templates/ButtonTemplates/AddButton.jsx
@@ -1,14 +1,13 @@
 import { TranslatableString } from '@rjsf/utils';
 import { Button } from '@wordpress/components';
 
-import IconButton from './IconButton';
 /** The `AddButton` renders a button that represent the `Add` action on a form
  */
 export default function AddButton({ className, onClick, disabled, registry, }) {
     const { translateString } = registry;
     return (<div className='row'>
       <p className={`add-button ${className}`}>
-        <Button iconType='info' icon='plus' title={translateString(TranslatableString.AddButton)} onClick={onClick} disabled={disabled} registry={registry}>Add</Button>
+        <Button  icon='plus' title={translateString(TranslatableString.AddButton)} onClick={onClick} disabled={disabled} registry={registry}>Add</Button>
       </p>
     </div>);
 }
--- packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/templates/ButtonTemplates/IconButton.jsx
+++ packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/templates/ButtonTemplates/IconButton.jsx
@@ -1,12 +1,6 @@
 import { TranslatableString } from '@rjsf/utils';
 import { Button } from '@wordpress/components';
 
-export default function IconButton(props) {
-    const { iconType = 'default', icon, className, uiSchema, registry, ...otherProps } = props;
-    return (<button type='button' className={`btn btn-${iconType} ${className}`} {...otherProps}>
-      <i className={`glyphicon glyphicon-${icon}`}/>
-    </button>);
-}
 export function CopyButton(props) {
     const { registry: { translateString }, } = props;
     return (<Button title={translateString(TranslatableString.CopyButton)} className='array-item-copy' {...props} icon='copy'/>);
