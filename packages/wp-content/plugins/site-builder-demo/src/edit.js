9d9957ac4e4108b0ba4e6b3a041633479ef0e031
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2024-03-17 12:41:36 +0100

    More hacky code

--- packages/wp-content/plugins/site-builder-demo/src/edit.js
+++ packages/wp-content/plugins/site-builder-demo/src/edit.js
@@ -26,23 +26,15 @@ export default function Edit({ setAttributes, attributes }) {
 
   const [ meta, setMeta ] = useEntityProp( 'postType', postType, 'meta' );
 
-  const metaValue = '{}';
-  const updateMetaValue = ( newValue ) => setMeta( { ...meta, 'metabox-block-data' : newValue } );
-
-  const [ intermediateValue, setIntermediateValue ] = useState(JSON.stringify(JSON.parse( metaValue), null, 2));
-  const textareaRef = useRef();
+  let formData = {};
+  Object.keys(attributes.schema.properties).map((property) => {
+    formData[property] = meta ? meta[property] : null;
+  });
 
   const onChange = (value) => {
-    try {
-      const object = JSON.parse(value);
-      updateMetaValue(JSON.stringify(object, null, 2));
-      textareaRef.current?.setCustomValidity('');
-    } catch(ex) {
-      textareaRef.current?.setCustomValidity(ex.message);
-    }
-    setIntermediateValue(value);
+    console.log( value );
+    setMeta( { ...meta, ...value } )
   };
-  console.log(attributes);
 
   return (
     <div { ...blockProps }>
@@ -50,8 +42,8 @@ export default function Edit({ setAttributes, attributes }) {
         schema={ attributes.schema }
         ui_schema={ attributes.ui_schema }
         validator={ validator }
-        formData={ attributes.formData }
-        onChange={ (e) => setAttributes({ formData: e.formData }) }
+        formData={ formData }
+        onChange={ (e) => onChange(e.formData) }
       />
     </div>
   );
