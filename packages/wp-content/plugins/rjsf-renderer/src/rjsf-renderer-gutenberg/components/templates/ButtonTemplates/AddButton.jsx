30bb3582a24c58591a65e5a88a963910595df9f0
Maximilian Andre <maximilian.andre@e-mundo.de>
2024-03-17 12:00:18 +0100

    fix(AddButton): Reverted wrong change

--- packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/templates/ButtonTemplates/AddButton.jsx
+++ packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/templates/ButtonTemplates/AddButton.jsx
@@ -8,7 +8,7 @@ export default function AddButton({ className, onClick, disabled, registry, }) {
     const { translateString } = registry;
     return (<div className='row'>
       <p className={`add-button ${className}`}>
-        <Button iconType='plusCircle' icon='plus' className='btn-add col-xs-12' title={translateString(TranslatableString.AddButton)} onClick={onClick} disabled={disabled} registry={registry}>Add</Button>
+        <Button iconType='info' icon='plus' title={translateString(TranslatableString.AddButton)} onClick={onClick} disabled={disabled} registry={registry}>Add</Button>
       </p>
     </div>);
 }
