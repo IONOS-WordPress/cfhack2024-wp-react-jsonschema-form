97551790b302ba09fb2755e676a5132b91e7949e
Maximilian Andre <maximilian.andre@e-mundo.de>
2024-03-17 11:36:11 +0100

    feat(AddButton): Changed AddButton to gutenberg

--- packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/templates/ButtonTemplates/AddButton.jsx
+++ packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/templates/ButtonTemplates/AddButton.jsx
@@ -1,12 +1,14 @@
 import { TranslatableString } from '@rjsf/utils';
+import { Button } from '@wordpress/components';
+
 import IconButton from './IconButton';
 /** The `AddButton` renders a button that represent the `Add` action on a form
  */
 export default function AddButton({ className, onClick, disabled, registry, }) {
     const { translateString } = registry;
     return (<div className='row'>
-      <p className={`col-xs-3 col-xs-offset-9 text-right ${className}`}>
-        <IconButton iconType='info' icon='plus' className='btn-add col-xs-12' title={translateString(TranslatableString.AddButton)} onClick={onClick} disabled={disabled} registry={registry}/>
+      <p className={`add-button ${className}`}>
+        <Button iconType='info' icon='plus' className='btn-add col-xs-12' title={translateString(TranslatableString.AddButton)} onClick={onClick} disabled={disabled} registry={registry}>Add</Button>
       </p>
     </div>);
 }
