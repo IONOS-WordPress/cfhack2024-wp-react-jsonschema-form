b3c44aedfdb2f3110dd26d21ffca5738ccf56e90
Michael Schmitz <michael+work@familyschmitz.de>
2024-03-17 11:00:57 +0100

    feat(rjsf-renderer): add gutenberg dateTime rendering

--- packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/widgets/DateTimeWidget.jsx
+++ packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/widgets/DateTimeWidget.jsx
@@ -1,11 +1,22 @@
-import { getTemplate, localToUTC, utcToLocal, } from '@rjsf/utils';
-/** The `DateTimeWidget` component uses the `BaseInputTemplate` changing the type to `datetime-local` and transforms
- * the value to/from utc using the appropriate utility functions.
+import { DateTimePicker } from '@wordpress/components';
+import { descriptionId, getTemplate, labelValue } from '@rjsf/utils';
+/** The `DateTimeWidget` component uses the `DateTimePicker`.
  *
  * @param props - The `WidgetProps` for this component
  */
-export default function DateTimeWidget(props) {
-    const { onChange, value, options, registry } = props;
-    const BaseInputTemplate = getTemplate('BaseInputTemplate', registry, options);
-    return (<BaseInputTemplate type='datetime-local' {...props} value={utcToLocal(value)} onChange={(value) => onChange(localToUTC(value))}/>);
+export default function DateTimeWidget({ schema, uiSchema, options, id, value, disabled, readonly, label, hideLabel, autofocus = false, onBlur, onFocus, onChange, registry, }) {
+    const DescriptionFieldTemplate = getTemplate('DescriptionFieldTemplate', registry, options);
+    const description = options.description ?? schema.description;
+    let startOfWeek = uiSchema.startOfWeek ?? 0;
+    startOfWeek = startOfWeek < 0 ? 0 : startOfWeek > 6 ? 6 : startOfWeek;
+    return (<>
+        {labelValue(<span>{label}</span>, hideLabel)}
+        <DateTimePicker
+            onChange={onChange}
+            is12Hour={uiSchema.is12Hour == true}
+            startOfWeek={startOfWeek}
+        />
+        {!hideLabel && !!description && (<DescriptionFieldTemplate id={descriptionId(id)} description={description} schema={schema} uiSchema={uiSchema} registry={registry} />)}
+    </>
+    );
 }
