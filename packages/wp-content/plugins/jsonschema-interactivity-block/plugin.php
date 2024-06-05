f2721d4c980a757cb55ad4ba4e08208a157ad4ee
Lars Gersmann <lars.gersmann@ionos.com>
2024-06-05 15:24:58 +0200

    wip: added skeleton for the jsonschema-interactivity-block plugin

---
+++ packages/wp-content/plugins/jsonschema-interactivity-block/plugin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name:       jsonschema-interactivity-block
+ * Description:       provides a Gutenberg block for configuring a form utilizing react-jsonschema-form and rendering the form using WordPress interactivity API
+ * Requires at least: 6.4
+ * Requires Plugins:  rjsf-renderer
+ * Requires PHP:      8.0
+ * Version:           1.0.0
+ * Author:            The Hackathon Project Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       settings-page
+ */
+
+namespace cfhack2024_wp_react_jsonschema_form\jsonschema_interactivity_block;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+
