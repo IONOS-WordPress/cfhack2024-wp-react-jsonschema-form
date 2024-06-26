f0e54a62861201e38bfc26f281951272f9d5de48
Lars Gersmann <lars.gersmann@ionos.com>
2024-06-26 13:56:22 +0200

    fix: fixed Text input

--- packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/widgets/TextWidget.jsx
+++ packages/wp-content/plugins/rjsf-renderer/src/rjsf-renderer-gutenberg/components/widgets/TextWidget.jsx
@@ -1,6 +1,6 @@
 import { TextControl } from '@wordpress/components';
 import { useCallback } from 'react';
-import { labelValue, schemaRequiresTrueValue, getUiOptions, } from '@rjsf/utils';
+import { labelValue, schemaRequiresTrueValue, getUiOptions, getInputProps} from '@rjsf/utils';
 import Markdown from 'markdown-to-jsx';
 
 /** The `TextWidget` component uses the `TextControl`.
@@ -8,28 +8,61 @@ import Markdown from 'markdown-to-jsx';
  * @param props - The `WidgetProps` for this component
  */
 export default function TextWidget(props) {
+  const { id, name, // remove this from ...rest
+  value, readonly, disabled, autofocus, className, onBlur, onFocus, onChange, onChangeOverride, options, schema, uiSchema, formContext, registry, rawErrors, type, hideLabel, // remove this from ...rest
+  hideError, // remove this from ...rest
+  ...rest } = props;
+  // Note: since React 15.2.0 we can't forward unknown element attributes, so we
+  // exclude the "options" and "schema" ones here.
+  if (!id) {
+      console.log('No id for', props);
+      throw new Error(`no id for props ${JSON.stringify(props)}`);
+  }
+  const inputProps = {
+      ...rest,
+      ...getInputProps(schema, type, options),
+  };
+  let inputValue;
+  if (inputProps.type === 'number' || inputProps.type === 'integer') {
+      inputValue = value || value === 0 ? value : '';
+  }
+  else {
+      inputValue = value == null ? '' : value;
+  }
+  const _onChange = useCallback(( value) => onChange(value === '' ? options.emptyValue : value), [onChange, options]);
+  const _onBlur = useCallback(( value) => onBlur(id, value), [onBlur, id]);
+  const _onFocus = useCallback((value) => onFocus(id, value), [onFocus, id]);
+
   const required = schemaRequiresTrueValue(props.schema);
 
   const uiOptions = getUiOptions(props.uiSchema);
 
-  const description = props.options.descdebuggerription || props.schema.description || '';
+  const description = props.options.description || props.schema.description || '';
   const richDescription = uiOptions.enableMarkdownInDescription ? <Markdown>{description}</Markdown> : description;
-
-  const handleChange = useCallback((value) => props.onChange(value === '' ? props.options.emptyValue : value));
 
   return (
     <TextControl
       label={labelValue(<span>{props.label}</span>, props.hideLabel)}
-      value={props.value}
-      className={props.className}
-      onChange={handleChange}
-      size={props.schema.maxLength}
-      maxLength={props.schema.maxLength}
-      onBlur={props.onBlur}
-      onFocus={props.onFocus}
-      readOnly={props.readOnly}
+      value={inputValue}
+      className={className}
+      onChange={_onChange}
+      size={schema.maxLength}
+      maxLength={schema.maxLength}
+      onBlur={_onBlur}
+      onFocus={_onFocus}
+      readOnly={readonly}
       default={props.default}
-      type={props.uiSchema.inputType ?? 'text'}
+      type={uiSchema.inputType ?? 'text'}
       help={richDescription}
       required={required} />);
+      /*
+      TODO: Implement examples
+      {Array.isArray(schema.examples) && (<datalist key={`datalist_${id}`} id={examplesId(id)}>
+          {schema.examples
+                .concat(schema.default && !schema.examples.includes(schema.default) ? [schema.default] : [])
+                .map((example) => {
+                return <option key={example} value={example}/>;
+            })}
+        </datalist>)}
+      */
 }
